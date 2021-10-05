552d5ba83ce8b3d5e4dff0f42aced9447d260088
javahirm <javahirmarjanyan@yahoo.com>
2021-10-05 22:27:26 +0400

    * two men shot Coca-Cola cans, how many missed each

---
+++ Homeworks/homework_1/shots.py
@@ -0,0 +1,5 @@
+first_shot = int(input("First man shot: "))
+second_shot = int(input("Second man shot: "))
+print("First man missed: ", second_shot - 1)
+print("Second man missed: ", first_shot - 1)
+
