b2154f4fc399766e7c7166b06762e72ec591ee93
javahirm <javahirmarjanyan@yahoo.com>
2021-10-31 23:53:45 +0400

    * string power problem

---
+++ Homeworks/homework_4/string_power.py
@@ -0,0 +1,27 @@
+s = input("Write something: ")
+k = int(input("Integer: "))
+length = len(s)
+if k >= 0:
+    print(s * k)
+else:
+    base = length / (-k)
+    if base % base != 0:
+        print("undefined")
+    else:
+        parts = [s[i: i + int(base)] for i in range(0, length, int(base))]
+        first = parts[0]
+        flag = True
+        for elem in parts:
+            if elem == first:
+                flag = True
+            else:
+                flag = False
+                break
+        if flag is True:
+            print(parts[0])
+        else:
+            print("undefined")
+
+
+
+
